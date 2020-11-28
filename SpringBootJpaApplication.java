2fdbe86d6f6d87e8cd9f4319a2a0a2e2517ac18a
Aditya Nambiar <46171665+aditnambiar@users.noreply.github.com>
2020-11-28 16:12:00 +0530

    Add files via upload

--- SpringBootJpaApplication.java
+++ SpringBootJpaApplication.java
@@ -2,13 +2,23 @@ package com.cred.io;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
 public class SpringBootJpaApplication {
 	
-	  public static void main(String[] args) {
-	    SpringApplication.run(SpringBootJpaApplication.class);
-	  }
+	@Bean
+	public RestTemplate getRestTemplate() {
+		
+		return new RestTemplate();
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaApplication.class);
+	}
 
 }
